84e4b3b5d96267857c8797f1a54e479bea96d2c6
NimayNdolo <nemendolo@gmail.com>
2021-06-24 16:37:44 -0400

    Add much needed styling changes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,9 +4,12 @@ const events = require('./auth/events')
 
 $(() => {
   $('#signOut').hide()
+  $('.circle').show()
   $('#game-board').hide()
   $('#newGame').hide()
+  $('#newGame2').hide()
   $('#newGame').on('click', events.onNewGame)
+  $('#newGame2').on('click', events.onNewGame)
   $('#signUp').on('submit', events.onSignUp)
   $('#signIn').on('submit', events.onSignIn)
   $('#signOut').on('click', events.onSignOut)
